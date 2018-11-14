f0a55771688d61dbfc3568378dd45fa83248b9ab
ramadhanAfwan <rama.afwan.mutasodirin3@gmail.com>
2018-11-14 14:37:44 +0700

    tambah reagen

---
+++ src/main/java/com/apap/TAsilab/controller/ReagenController.java
@@ -0,0 +1,10 @@
+package com.apap.TAsilab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lab/kebutuhan")
+public class ReagenController {
+
+}
