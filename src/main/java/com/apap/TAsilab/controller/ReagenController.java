1edb5840a01de81c062ee99a8b53dfe2210dbfdd
Aldi Firmansyah <aldifirmansyh@gmail.com>
2018-11-14 14:54:26 +0700

    buat service dan repository reagen

--- src/main/java/com/apap/TAsilab/controller/ReagenController.java
+++ src/main/java/com/apap/TAsilab/controller/ReagenController.java
@@ -1,10 +1,14 @@
 package com.apap.TAsilab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.apap.TAsilab.service.KebutuhanReagenService;
 
 @Controller
 @RequestMapping("/lab/kebutuhan")
 public class ReagenController {
-
+	@Autowired
+	KebutuhanReagenService kebutuhanReagenService;
 }
---
+++ src/main/java/com/apap/TAsilab/service/KebutuhanReagenServiceImpl.java
@@ -0,0 +1,14 @@
+package com.apap.TAsilab.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.TAsilab.repository.KebutuhanReagenDB;
+
+@Service
+@Transactional
+public class KebutuhanReagenServiceImpl implements KebutuhanReagenService {
+	@Autowired
+	private KebutuhanReagenDB kebutuhanReagenDB;
+}
