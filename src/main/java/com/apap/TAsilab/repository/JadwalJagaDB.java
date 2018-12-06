2b3b5b1581dfa8d145ecd2edd935ccc26bcb1dba
ghalsa <ghina.almira.salsabila@gmail.com>
2018-12-06 15:03:52 +0700

    tambahan fitur 11-13

--- src/main/java/com/apap/TAsilab/repository/JadwalJagaDB.java
+++ src/main/java/com/apap/TAsilab/repository/JadwalJagaDB.java
@@ -1,8 +1,14 @@
 package com.apap.TAsilab.repository;
 import com.apap.TAsilab.model.JadwalJagaModel;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface JadwalJagaDB extends JpaRepository<JadwalJagaModel, Long>{
 
+	List<JadwalJagaModel> findByTanggal(String tanggal);
+	JadwalJagaModel findById(long id);
 }
--- src/main/java/com/apap/TAsilab/service/JadwalJagaService.java
+++ src/main/java/com/apap/TAsilab/service/JadwalJagaService.java
@@ -1,11 +1,19 @@
 package com.apap.TAsilab.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
-
 import com.apap.TAsilab.model.JadwalJagaModel;
 
 @Service
 public interface JadwalJagaService {
 
 	void addJadwalJaga(JadwalJagaModel jadwalJaga);
+
+	List<JadwalJagaModel> getJadwalJagaByTangal(String tanggal);
+
+	JadwalJagaModel getJadwalJagaById(long id);
+
+	void ubahJadwalJaga(long id, JadwalJagaModel newJadwalJaga);
+	
+	
 }
