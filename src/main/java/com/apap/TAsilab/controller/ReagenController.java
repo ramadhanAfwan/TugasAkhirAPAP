ad140442469885ba41ec83b1c956cc8a96cdfbea
Aldi Firmansyah <aldifirmansyh@gmail.com>
2018-12-08 01:00:52 +0700

    fitur 6 aldi selesai, insya Allah semua fitur aldi beres

--- src/main/java/com/apap/TAsilab/controller/ReagenController.java
+++ src/main/java/com/apap/TAsilab/controller/ReagenController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.apap.TAsilab.model.KebutuhanReagenModel;
+import com.apap.TAsilab.model.UserRoleModel;
 import com.apap.TAsilab.service.KebutuhanReagenService;
 import com.apap.TAsilab.service.LabSuppliesService;
+import com.apap.TAsilab.service.UserRoleService;
 
 @Controller
 @RequestMapping("/lab/kebutuhan")
@@ -26,8 +30,11 @@ public class ReagenController {
 	@Autowired
 	LabSuppliesService labSuppliesService;
 	
+	@Autowired
+	UserRoleService userRoleService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	private String addUserSubmit(@ModelAttribute KebutuhanReagenModel reagen, Model model) {
+	private String viewAllKebutuhanReagen(@ModelAttribute KebutuhanReagenModel reagen, Model model) {
 		List<KebutuhanReagenModel> listKebutuhanReagen = kebutuhanReagenService.findAll();
 		if(listKebutuhanReagen.size()==0) {
 			model.addAttribute("header", "Perencanaan Kebutuhan Reagen Belum Diajukan");
@@ -40,6 +47,12 @@ public class ReagenController {
 		// Untuk tambah baru
 		model.addAttribute("listReagen", labSuppliesService.getAllReagen());
 		model.addAttribute("kebutuhanReagen", new KebutuhanReagenModel());
+		
+		
+		// Role
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		UserRoleModel user = userRoleService.findUserByUsername(auth.getName());
+		model.addAttribute("role", user.getRole());
 		
 		return "lihat-kebutuhan-reagen";
 	}
@@ -63,9 +76,30 @@ public class ReagenController {
 
 	@RequestMapping(value = "/ubah/{id}", method = RequestMethod.GET)
 	public String ubahStatus(@PathVariable(value= "id") int idReagen, Model model) {
-		Optional<KebutuhanReagenModel> reagen = kebutuhanReagenService.findReagenById(idReagen);
-		model.addAttribute("reagen", reagen);
+		KebutuhanReagenModel kebutuhanReagen = kebutuhanReagenService.findReagenById(idReagen).get();
+		model.addAttribute("kebutuhanReagen", kebutuhanReagen);
 		model.addAttribute("title", "Ubah Status Kebutuhan Reagen");
 		return "ubah-kebutuhan-reagen";
+	}
+	
+	@RequestMapping(value = "/ubah", method = RequestMethod.POST)
+	public String submitUbahStatus(@ModelAttribute KebutuhanReagenModel kebutuhanReagen, Model model) {
+		int statusTemp = kebutuhanReagen.getStatus();
+		kebutuhanReagen = kebutuhanReagenService.findReagenById(kebutuhanReagen.getId()).get();
+		
+		if (statusTemp == kebutuhanReagen.getStatus()) {
+			model.addAttribute("msg", "Harap ubah status kebutuhan reagen terlebih dahulu.");
+		}
+		else {
+			kebutuhanReagen.setStatus(statusTemp);
+			
+			long millis=System.currentTimeMillis();
+			Date todayDate = new Date(millis);
+			kebutuhanReagen.setTanggalUpdate(todayDate);
+			
+			kebutuhanReagenService.add(kebutuhanReagen);
+			model.addAttribute("msg", "Status perencanaan kebutuhan reagen berhasil diubah.");
+		}
+		return "success-page";
 	}
 }
